7437b60b0f97b101e471bf4a8eb26cfc0d3c3cd8
Hardeep Shoker <shokerhardeep@gmail.com>
2019-03-08 22:12:11 -0800

    Updating references

--- src/noFocusedTestsRule.ts
+++ src/noFocusedTestsRule.ts
@@ -3,7 +3,6 @@ import * as ts from "typescript";
 import {Walk} from "./noFocusedTestsWalk";
 
 export class Rule extends Lint.Rules.AbstractRule {
-    public static FAILURE_STRING = "Unexpected focused test.";
 
     public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
         return this.applyWithWalker(new Walk(sourceFile, this.getOptions()));
--- src/noFocusedTestsWalk.ts
+++ src/noFocusedTestsWalk.ts
@@ -1,11 +1,16 @@
+import * as Lint from "tslint";
+import * as ts from "typescript";
+
 export class Walk extends Lint.RuleWalker {
+
+    public static FAILURE_STRING = "Unexpected focused test.";
 
     protected assertionChecks = [/\.only$/, /^ftest$/, /^fit$/, /^fdescribe$/];
 
     protected visitCallExpression(node: ts.CallExpression) {
         this.assertionChecks.forEach(exp => {
             if (node.expression.getText().match(exp)) {
-                this.addFailureAt(node.getStart(), node.getEnd() - node.getStart(), Rule.FAILURE_STRING);
+                this.addFailureAt(node.getStart(), node.getEnd() - node.getStart(), Walk.FAILURE_STRING);
             }
         });
         super.visitCallExpression(node);
